ddf25b4f9fa6e368610a04ded1d7e7c8c6a3393e
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-08-12 22:27:39 +0530

    added

--- src/lib/components/FeedBack.svelte
+++ src/lib/components/FeedBack.svelte
@@ -7,73 +7,51 @@
 
 	let cards: HTMLDivElement[] = [];
 
-	const cardData = [
-		{ rotate: 5 },
-		{ rotate: -8 },
-		{ rotate: 3 }
-	];
-
 	onMount(() => {
 		cards.forEach((card, i) => {
-			gsap.set(card, {
-				rotation: cardData[i].rotate,
-				y: i * 2, // tiny offset so they aren't pixel-perfect
-				zIndex: cardData.length - i
-			});
-		});
-
-		// Animate each card away on scroll
-		cards.forEach((card, i) => {
-			if (i === cards.length - 1) return; // last card stays
-
-			gsap.to(card, {
-				y: '-120%',
-				rotation: cardData[i].rotate + gsap.utils.random(-10, 10),
-				opacity: 0,
-				scrollTrigger: {
-					trigger: card,
-					start: 'top center+=100',
-					end: 'bottom center',
-					scrub: true
-				}
+			ScrollTrigger.create({
+				trigger: card,
+				start: 'top center',
+				end: '+=200%',
+				pin: true,
+				pinSpacing: false,
+				onEnter: () => gsap.to(card, { autoAlpha: 1, y: 0, duration: 0.6 }),
+				onLeaveBack: () => gsap.to(card, { autoAlpha: 0, y: 50, duration: 0.4 })
 			});
 		});
 	});
 </script>
 
-<div class="relative flex  min-h-[calc(100vh*2)] items-center justify-center bg-gray-100">
-	<div class="relative w-full max-w-[500px] h-[640px]">
-		{#each cardData as { rotate }, i}
-			<div
-				bind:this={cards[i]}
-				class="absolute top-0 left-0 h-full w-full rounded bg-[#f7f3ed] p-2 shadow-lg"
-			>
-				<figure class="flex h-[325px] w-full overflow-hidden rounded">
-					<img
-						src="https://madeinuxstudio.com/_next/image?url=https%3A%2F%2Fimages.prismic.io%2Fmiux-studio%2FaA3mcvIqRLdaBpIJ_client3.jpg%3Fauto%3Dformat%2Ccompress&w=1080&q=90"
-						alt=""
-						class="h-full w-full object-cover"
-					/>
-				</figure>
-				<div class="p-4">
-					<p class="text-center">xxFullNamexx <span></span> xxTitlexx</p>
-					<p class="text-center">xxBrandxx</p>
+<section class="relative min-h-screen w-ful">
+	
+		<div
+			class="absolute inset-0 bg-cover bg-fixed bg-center"
+			style={`background-image: url(https://madeinuxstudio.com/_next/image?url=https%3A%2F%2Fimages.prismic.io%2Fmiux-studio%2FaDYL_idWJ-7kSmR-_Testimonial.jpg%3Fauto%3Dformat%2Ccompress&w=1920&q=90`}
+		></div>
 
-					<figure class="mx-auto flex h-[72px] w-[167px] items-center justify-center">
+		<div class="mx-auto flex max-w-3xl flex-col items-center">
+			{#each [{ rotate: 'rotate-2' }, { rotate: '-rotate-2' }, { rotate: 'rotate-1' }] as { rotate }, i}
+				<div
+					bind:this={cards[i]}
+					class={`sticky top-20 my-10 w-full max-w-xl translate-y-12 transform rounded-xl bg-white p-4 opacity-0 shadow-lg ${rotate}`}
+				>
+					<figure class="h-64 w-full overflow-hidden rounded-lg">
 						<img
-							src="https://miux-studio.cdn.prismic.io/miux-studio/aA_QuPIqRLdaBrw2_sign-about-3.svg"
+							src="https://picsum.photos/600/400?random={i}"
 							alt=""
-							class="h-full w-full"
+							class="h-full w-full object-cover"
 						/>
 					</figure>
-
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab tempore consectetur ut suscipit
-						libero eos natus asperiores in molestiae distinctio, sit accusantium quaerat repudiandae
-						aliquam, placeat cupiditate deleniti earum sequi nisi. Voluptatem!
-					</p>
+					<div class="mt-4 space-y-2">
+						<p class="text-center text-lg font-bold">Full Name {i + 1}</p>
+						<p class="text-center text-sm text-gray-500">Brand / Role</p>
+						<p class="text-gray-700">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos incidunt beatae ullam
+							reiciendis soluta quae expedita pariatur suscipit tempora nostrum!
+						</p>
+					</div>
 				</div>
-			</div>
-		{/each}
-	</div>
-</div>
+			{/each}
+		</div>
+	
+</section>
