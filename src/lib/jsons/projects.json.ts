d233eda0a2deb71472685f5edea24e4bb3fea707
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-08-13 22:47:14 +0530

    added

--- src/lib/jsons/projects.json.ts
+++ src/lib/jsons/projects.json.ts
@@ -4,55 +4,41 @@ export const projects = [
         title: 'Project One',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=1'
+        projectImage: 'https://fastly.picsum.photos/id/688/600/400.jpg?hmac=M014VOlusw1Md_8vEx6Tk-GIDuv1VHvgcTTNGGzt4Ac'
     },
     {
         id: 2,
         title: 'Project Two',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=2'
+        projectImage: 'https://fastly.picsum.photos/id/564/600/400.jpg?hmac=OmYp9cwndfkSfjpoQ7sCXjADuN_e5wn9g2iaCvX2Iic'
     },
     {
         id: 3,
         title: 'Project Three',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=3'
+        projectImage: 'https://fastly.picsum.photos/id/758/600/400.jpg?hmac=QUxmK4S6FIIf7YSIWdqNdFhCSjlwRs0jq5B6vcQChQQ'
     },
     {
         id: 4,
         title: 'Project Four',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=4'
+        projectImage: 'https://fastly.picsum.photos/id/117/600/400.jpg?hmac=cW1yapIiMyNTnd1eWtFGxL1wC9HzTNBrsOqB56bSFKM'
     },
     {
         id: 5,
         title: 'Project Five',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=5'
+        projectImage: 'https://fastly.picsum.photos/id/467/600/400.jpg?hmac=8JqMpI-jCbRKN_Qs1x9vdYC9fi8UlAYarTxiut_VBtc'
     },
     {
         id: 6,
         title: 'Project Six',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=6'
+        projectImage: 'https://fastly.picsum.photos/id/865/600/400.jpg?hmac=hRQRROKYp2k1uokEDmWWgJIyVCR0XChyen3JIs0evkA'
     },
-    {
-        id: 7,
-        title: 'Project Seven',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=7'
-    },
-    {
-        id: 8,
-        title: 'Project Eight',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae sapien nec magna viverra scelerisque.',
-        projectImage: 'https://picsum.photos/600/400?random=8'
-    }
 ];
